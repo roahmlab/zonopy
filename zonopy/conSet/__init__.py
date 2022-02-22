41edab7b44e32f5cc6d922b36bbc628b42054d99
Yongseok Kwon <kwonys@umich.edu>
2022-02-21 21:45:34 -0500

    set up

---
+++ zonopy/conSet/__init__.py
@@ -0,0 +1,3 @@
+from zonopy.conSet.zonotope.zono import zonotope
+from zonopy.conSet.polynomial_zonotope.poly_zono import polyZonotope
+from zonopy.conSet.polynomial_zonotope.mat_poly_zono import matPolyZonotope
