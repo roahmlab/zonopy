0d7987178e6c92ff4ad5d497db31a7f8f5cac435
Yongseok Kwon <kwonys@umich.edu>
2022-02-21 21:24:32 -0500

    load jrs

--- zonopy/load_jrs/load_jrs.py
+++ zonopy/load_jrs/load_jrs.py
@@ -3,18 +3,44 @@ import torch
 from .. import zonotope
 from mat4py import loadmat
 
-T_plan = 0.5
-T_total = 1
+T_fail_safe = 0.5
+jrs_path = './jrs_mat_saved/'
+jrs_key = loadmat(jrs_path+'c_kvi.mat')
+jrs_key = torch.tensor(jrs_key['c_kvi'])
+ka_dim = 3
+acc_dim = 3 
+kv_dim = 4
 
 def load_JRS(qpos,qvel):
-    jrs_path = './jrs_mat_saved/'
-    jrs_key = loadmat(jrs_path+'c_kvi.mat')
-    jrs_key = torch.tensor(jrs_key['c_kvi'])
+    '''
+    load joint reachable set precomputed by MATLAB CORA (look gen_jrs).
+    Then, operate loaded JRS zonotope into JRS polyzonotope w/ k-sliceable dep. gen. 
+    for initial joint pos. and intial joint vel.
 
-    time_idx = 5 # 6-1
-    k_idx = 3 # 4-1
-    acc_idx = 3 #4-1
-    JRS = {}
+    qpos: <torch.Tensor> initial joint position
+    , size [N]
+    qvel: <torch.Tensor> initial joint velocity
+    , size [N]
+
+    return <dict>, <polyZonotope> dictionary of polynomical zonotope
+    JRS_poly(i,t)
+    - i: i-th joint
+    - t: t-th timestep \in [0,99] -> 0 ~ 1 sec
+    
+
+    ** dimension index
+    0: cos(qpos_i)
+    1: sin(qpos_i)
+    2: qvel_i
+    3: ka_i
+    4: kv_i
+    5: t
+
+    '''
+    assert len(qpos.shape) == 1 and len(qvel.shape) == 1 
+    assert len(qpos) == len(qvel)
+
+    JRS_poly = {}
     for i in range(len(qvel)):
         closest_idx = torch.argmin(abs(qvel[i]-jrs_key))
         jrs_filename = jrs_path+'JRS_mat_'+format(jrs_key[closest_idx],'.3f')+'.mat'
@@ -30,25 +56,14 @@ def load_JRS(qpos,qvel):
             Rot_qpos = torch.tensor([[c_qpos,-s_qpos],[s_qpos,c_qpos]])
             A = torch.block_diag(Rot_qpos,torch.eye(4))
             JRS_zono_i = zonotope(jrs_mat_load[jrs_idx])
-            JRS[(jrs_idx,i)] = A @ slice(JRS_zono_i,5,qvel[i])
-
-
-        # fail-safe plan
-        G = JRS[(0,i)].Z[k_idx,1:]
-        k_idx = (G!=0).nonzero()[0]
-        if k_idx.numel() != 1:
-            if k_idx.numel() == 1:
-                raise ValueError('no k-sliceable generator for slice index')
-            else:
-                raise ValueError('more than one no k-sliceable generators')
-
-        delta_k = G[k_idx]
-        c_braking = -qvel[i]/0.5
-        delta_breaking = -delta_k/0.5
-        for jrs_idx in range(int(n_time_steps/2),n_time_steps):
-            Z = JRS[(jrs_idx,i)].Z
-            Z[acc_idx,0] = c_braking
-            Z[acc_idx,k_idx+1] = delta_breaking
-            JRS[jrs_idx,i] = zonotope(Z)
-
-    return JRS
+            JRS_zono_i = A @ JRS_zono_i.slice(kv_dim,qvel[i])
+            JRS_poly[(i,jrs_idx)] = JRS_zono_i.to_polyZonotope(ka_dim)
+            # NOTE: fail safe
+            if jrs_idx == 0:
+                delta_k = JRS_poly[(i,0)].G[ka_dim,0]
+                c_breaking = - qvel[i]/T_fail_safe
+                delta_breaking = - delta_k/T_fail_safe
+            if jrs_idx >= int(n_time_steps/2):
+                JRS_poly[(i,jrs_idx)].c[acc_dim] = c_breaking
+                JRS_poly[(i,jrs_idx)].G[acc_dim] = delta_breaking
+    return JRS_poly
