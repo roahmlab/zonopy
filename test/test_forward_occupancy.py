49045cadb687155d5e0dfff3f9e6b58bc15db870
Yongseok Kwon <kwonys@umich.edu>
2022-02-22 03:20:52 -0500

    forward occupancy

---
+++ test/test_forward_occupancy.py
@@ -0,0 +1,28 @@
+
+
+import torch
+import zonopy
+import matplotlib.pyplot as plt
+from zonopy.forward_kinematics.FO import forward_occupancy
+qpos =  torch.tensor([0,0])
+qvel =  torch.tensor([1.2,1.2])
+joint_axes = [torch.tensor([0,0,1])]*2
+P = [torch.tensor([1,0,0])]*2
+link_zonos = [zonopy.zonotope(torch.tensor([[0.5,0.5,0],[0,0,0.01],[0,0,0]])).to_polyZonotope()]*2
+
+EE = forward_occupancy(qpos,qvel,joint_axes,P,link_zonos)
+
+max_key = max(EE.keys())
+n_joints = max_key[0]+1
+n_time_steps = max_key[1]+1
+
+fig = plt.figure()    
+ax = fig.gca() 
+
+for i in range(n_joints):
+    for t in range(n_time_steps):
+        Z = EE[(i,t)].to_zonotope()
+        Z.plot2d(ax,facecolor='none')
+
+plt.axis([-2,2,-2,2])
+plt.show()
--- zonopy/forward_kinematics/FO.py
+++ zonopy/forward_kinematics/FO.py
@@ -7,23 +7,24 @@ from zonopy.forward_kinematics.rotatotope import get_rotato_from_jrs
 
 dim = 3
 
-def forward_kinematics(qpos,qvel,joint_axes,P):
+def forward_occupancy(qpos,qvel,joint_axes,P,link_zonos):
     '''
     P: <list>
     '''
-    assert len(qpos) == len(qvel) and len(qpos) == len(joint_axes) and len(qpos) == len(P)
+    assert len(qpos) == len(qvel) and len(qpos) == len(joint_axes) and len(qpos) == len(P) and len(qpos) == len(link_zonos)
     JRS_poly = load_JRS(qpos,qvel)
     max_key = max(JRS_poly.keys())
     n_joints = max_key[0]+1
     n_time_steps = max_key[1]+1
     rotato = get_rotato_from_jrs(JRS_poly,joint_axes)
 
-    EE = {}
+    link_FO = {}
     for t in range(n_time_steps):
         for i in range(n_joints):
-            EE[(i,t)] = torch.zeros(3,dtype=torch.float32)
-            for j in reversed(range(i+1)):
-                EE[(i,t)] = rotato[(j,t)]@(EE[(i,t)]+P[j])
-    return EE
+            link_FO[(i,t)] = rotato[(i,t)]@link_zonos[i]
+            for j in reversed(range(i)):
+                link_FO[(i,t)] =rotato[(j,t)]@(link_FO[(i,t)]+P[j]) 
+
+    return link_FO
 
 
