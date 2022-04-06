89ee4a8620da03ef3cc3f822b766f425c774daa4
Yongseok Kwon <kwonys@umich.edu>
2022-04-06 16:48:51 -0400

    re-organize

--- zonopy/__init__.py
+++ zonopy/__init__.py
@@ -1,3 +1,21 @@
-from zonopy.conSet import zonotope, matZonotope, polyZonotope, matPolyZonotope
-from zonopy.load_jrs.load_jrs import load_JRS
+from zonopy.conSet.zonotope.zono import zonotope
+from zonopy.conSet.zonotope.mat_zono import matZonotope
+from zonopy.conSet.polynomial_zonotope.poly_zono import polyZonotope
+from zonopy.conSet.polynomial_zonotope.mat_poly_zono import matPolyZonotope
+from zonopy.conSet.interval.interval import interval
+
+from zonopy.joint_reachable_set.jrs_trig.load_jrs_trig import load_JRS_trig, load_traj_JRS_trig
+from zonopy.joint_reachable_set.gen_jrs import gen_JRS
+
 from zonopy.utils import *
+from zonopy.load_urdf.load_robot import *
+
+__version__ = "0.0.1"
+__logo__ = """
+*** ZONO-PY ***
+  _____
+ |     |__
+ |_____|  |__
+    |_____|  |
+       |_____|
+"""
