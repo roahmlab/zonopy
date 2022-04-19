d54336b03443ed4393ba5fc6db8067cc8662dc67
Yongseok Kwon <kwonys@umich.edu>
2022-04-18 21:16:55 -0400

    kinematics

--- zonopy/kinematics/FK.py
+++ zonopy/kinematics/FK.py
@@ -5,13 +5,16 @@ def forward_kinematics(rotatos,robot_params):
     '''
     P: <list>
     '''
+    zono_order= 40
     n_joints = robot_params['n_joints']
     P = robot_params['P']
-
+    R = robot_params['R']
     P_motor = [polyZonotope(torch.zeros(3,dtype=torch.float32))]
     R_motor = [matPolyZonotope(torch.eye(3,dtype=torch.float32))]
     
     for i in range(n_joints):
-        P_motor.append(R_motor[-1]@P[i] + P_motor[-1])
-        R_motor.append(R_motor[-1]@rotatos[i])
+        P_motor_temp = R_motor[-1]@P[i] + P_motor[-1]
+        P_motor.append(P_motor_temp.reduce(zono_order))      
+        R_motor_temp = R_motor[-1]@R[i]@rotatos[i]
+        R_motor.append(R_motor_temp.reduce(zono_order))
     return R_motor[1:], P_motor[1:]
--- zonopy/kinematics/FO.py
+++ zonopy/kinematics/FO.py
@@ -8,14 +8,15 @@ def forward_occupancy(rotatos,link_zonos,robot_params):
     zono_order = 40
     n_joints = robot_params['n_joints']
     P = robot_params['P']
+    R = robot_params['R']
     P_motor = [polyZonotope(torch.zeros(3,dtype=torch.float32))]
     R_motor = [matPolyZonotope(torch.eye(3,dtype=torch.float32))]
     FO_link = []
     for i in range(n_joints):
         P_motor_temp = R_motor[-1]@P[i] + P_motor[-1]
         P_motor.append(P_motor_temp.reduce(zono_order))
-        R_motor_temp = R_motor[-1]@rotatos[i]
-        R_motor.append(R_motor_temp)
+        R_motor_temp = R_motor[-1]@R[i]@rotatos[i]
+        R_motor.append(R_motor_temp.reduce(zono_order))
         FO_link_temp = R_motor[-1]@link_zonos[i] + P_motor[-1]
         FO_link.append(FO_link_temp.reduce(zono_order))
     return FO_link, R_motor[1:], P_motor[1:]
