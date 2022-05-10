00d5f61fec5969b4ab8471b5af25e7fe7411855b
Yongseok Kwon <kwonys@umich.edu>
2022-05-09 21:01:36 -0400

    batch

--- test/test_bic_FO.py
+++ test/test_bic_FO.py
@@ -1,6 +1,6 @@
 import torch
 import zonopy as zp
-from zonopy.kinematics.FO import batch_forward_occupancy, forward_occupancy
+from zonopy.kinematics.FO import forward_occupancy
 import time
 zp.setup_cuda()
 batch_size = 32
@@ -14,22 +14,54 @@ params = {'joint_axes':[torch.tensor([0.0,0.0,1.0])]*N_joints,
         'n_joints':N_joints}
 link_zonos = [zp.zonotope(torch.tensor([[0.5,0.5,0.0],[0.0,0.0,0.01],[0.0,0.0,0.0]]).T).to_polyZonotope()]*N_joints # [
 
-
-qpos = torch.zeros(batch_size,N_joints)
-qvel = torch.pi/2*torch.ones(batch_size,N_joints)
+qpos = torch.rand(batch_size,N_joints)*2*torch.pi-torch.pi
+qvel = torch.rand(batch_size,N_joints)*2*torch.pi-torch.pi
 
 t_start = time.time()
-J1, R_trig1 = zp.load_batch_JRS_trig_ic(qpos,qvel)
+_, R_trig1 = zp.load_batch_JRS_trig_ic(qpos,qvel)
+FO_link1,_, _ = forward_occupancy(R_trig1,link_zonos,params)
 t =  time.time()
 print(t-t_start)
 t_start = t
 
 
-FO_link1,r_trig1, p_trig1 = batch_forward_occupancy(R_trig1,link_zonos,params)
+FO_link2 = []
+t_start = time.time()
+for i in range(batch_size):
+    _, R_trig2 = zp.load_batch_JRS_trig(qpos[i],qvel[i])
+    FO_link_temp,_, _ = forward_occupancy(R_trig2,link_zonos,params)
+    FO_link2.append(FO_link_temp)
+
 t =  time.time()
 print(t-t_start)
 t_start = t
+
+FO_link3 = [[] for _ in range(batch_size)]
+t_start = time.time()
+for i in range(batch_size):
+    _, R_trig3 = zp.load_JRS_trig(qpos[i],qvel[i])
+    for t in range(100):
+        FO_link_temp,_, _ = forward_occupancy(R_trig3[t],link_zonos,params)
+        FO_link3[i].append(FO_link_temp)
+
+t =  time.time()
+print(t-t_start)
+t_start = t
+
+plot_on = False
+if plot_on:
+    import matplotlib.pyplot as plt
+    for i in range(batch_size):
+        fig = plt.figure()
+        ax = fig.gca()
+        for j in range(N_joints):
+            for t in range(100):
+                FO_link1[j][i,t].to_zonotope().plot(ax)
+                FO_link2[i][j][t].to_zonotope().plot(ax,edgecolor='red')
+                FO_link3[i][t][j].to_zonotope().plot(ax,edgecolor='blue')
+        plt.autoscale()
+        plt.show()
+
+
 import pdb; pdb.set_trace()
-
-
 
