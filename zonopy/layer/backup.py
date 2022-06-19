c0c5bd3924166797fc91ab84db8ff50bb24d6200
Yongseok Kwon <kwonys@umich.edu>
2022-06-18 21:24:33 -0400

    merge multi processing

--- zonopy/layer/backup.py
+++ zonopy/layer/backup.py
@@ -13,7 +13,7 @@ T_PLAN, T_FULL = 0.5, 1.0
 
 # batch
 
-def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
+def gen_RTS_star_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
     jrs_tensor = preload_batch_JRS_trig()
     dimension = 2
     n_timesteps = 100
@@ -21,36 +21,35 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
     PI_vel = torch.tensor(torch.pi-1e-6)
     zono_order=40
     g_ka = torch.pi/24
-    class grad_RTS_2D_Layer(torch.autograd.Function):
+    class RTS_star_2D_Layer(torch.autograd.Function):
         @staticmethod
         def forward(ctx,lambd,observation):
             # observation = [ qpos | qvel | qgoal | obs_pos1,...,obs_posO | obs_size1,...,obs_sizeO ]
-            
             ctx.lambd_shape, ctx.obs_shape = lambd.shape, observation.shape
-            ctx.lambd =lambd.clone().reshape(-1,n_links).to(dtype=torch.get_default_dtype())             
+            lambd =lambd.reshape(-1,n_links).to(dtype=torch.get_default_dtype()) 
             #observation = observation.reshape(-1,observation.shape[-1]).to(dtype=torch.get_default_dtype())
             observation = observation.to(dtype=torch.get_default_dtype())
-            ka = g_ka*ctx.lambd
+            ka = g_ka*lambd
             
             n_batches = observation.shape[0]
-            ctx.qpos = observation[:,:n_links]
-            ctx.qvel = observation[:,n_links:2*n_links]
+            qpos = observation[:,:n_links]
+            qvel = observation[:,n_links:2*n_links]
             obstacle_pos = observation[:,-4*n_obs:-2*n_obs]
             obstacle_size = observation[:,-2*n_obs:]
-            qgoal = ctx.qpos + ctx.qvel*T_PLAN + 0.5*ka*T_PLAN**2
+            qgoal = qpos + qvel*T_PLAN + 0.5*ka*T_PLAN**2
 
             #g_ka = torch.maximum(PI/24,abs(qvel/3))
 
-            _, R_trig = process_batch_JRS_trig_ic(jrs_tensor,ctx.qpos,ctx.qvel,joint_axes)
+            _, R_trig = process_batch_JRS_trig_ic(jrs_tensor,qpos,qvel,joint_axes)
             FO_link,_,_ = forward_occupancy(R_trig,link_zonos,params)
             
             As = [[] for _ in range(n_links)]
             bs = [[] for _ in range(n_links)]
 
-            lambda_to_slc = ctx.lambd.reshape(n_batches,1,dimension).repeat(1,n_timesteps,1)
+            lambda_to_slc = lambd.reshape(n_batches,1,dimension).repeat(1,n_timesteps,1)
             
             #unsafe_flag = torch.zeros(n_batches) 
-            unsafe_flag = (abs(ctx.qvel+ctx.lambd*g_ka*T_PLAN)>PI_vel).any(-1)#NOTE: this might not work on gpu, velocity lim check
+            unsafe_flag = (abs(qvel+lambd*g_ka*T_PLAN)>PI_vel).any(-1)#NOTE: this might not work on gpu, velocity lim check
             for j in range(n_links):
                 FO_link[j] = FO_link[j].project([0,1]) 
                 c_k = FO_link[j].center_slice_all_dep(lambda_to_slc).unsqueeze(-1) # FOR, safety check
@@ -63,18 +62,17 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
 
             M_obs = n_timesteps*n_links*n_obs
             M = M_obs+2*n_links
-            ctx.flags = -torch.ones(n_batches,dtype=int) # -1: direct pass, 0: safe plan from armtd pass, 1: fail-safe plan from armtd pass
-            unsafe_flag += True
+            flags = -torch.ones(n_batches) # -1: direct pass, 0: safe plan from armtd pass, 1: fail-safe plan from armtd pass
             for i in unsafe_flag.nonzero().reshape(-1):
                 class nlp_setup():
                     x_prev = np.zeros(n_links)*np.nan
                     def objective(nlp,x):
-                        qplan = ctx.qpos[i] + ctx.qvel[i]*T_PLAN + 0.5*g_ka*x*T_PLAN**2
+                        qplan = qpos[i] + qvel[i]*T_PLAN + 0.5*x*T_PLAN**2
                         return torch.sum(wrap_to_pi(qplan-qgoal[i])**2)
 
                     def gradient(nlp,x):
-                        qplan = ctx.qpos[i] + ctx.qvel[i]*T_PLAN + 0.5*g_ka*x*T_PLAN**2
-                        return (g_ka*T_PLAN**2*wrap_to_pi(qplan-qgoal[i])).numpy()
+                        qplan = qpos[i] + qvel[i]*T_PLAN + 0.5*x*T_PLAN**2
+                        return (T_PLAN**2*wrap_to_pi(qplan-qgoal[i])).numpy()
 
                     def constraints(nlp,x): 
                         ka = torch.tensor(x,dtype=torch.get_default_dtype()).unsqueeze(0).repeat(n_timesteps,1)
@@ -82,7 +80,7 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                             cons_obs = torch.zeros(M)                   
                             grad_cons_obs = torch.zeros(M,n_links)
                             # velocity min max constraints
-                            possible_max_min_q_dot = torch.vstack((ctx.qvel[i],ctx.qvel[i]+g_ka*x*T_PLAN,torch.zeros_like(ctx.qvel[i])))
+                            possible_max_min_q_dot = torch.vstack((qvel[i],qvel[i]+x*T_PLAN,torch.zeros_like(qvel[i])))
                             q_dot_max, q_dot_max_idx = possible_max_min_q_dot.max(0)
                             q_dot_min, q_dot_min_idx = possible_max_min_q_dot.min(0)
                             grad_q_max = torch.diag(T_PLAN*(q_dot_max_idx%2))
@@ -91,8 +89,8 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                             grad_cons_obs[-2*n_links:] = torch.vstack((grad_q_max,grad_q_min))
                             # velocity min max constraints 
                             for j in range(n_links):
-                                c_k = FO_link[j][i].center_slice_all_dep(ka)
-                                grad_c_k = FO_link[j][i].grad_center_slice_all_dep(ka)
+                                c_k = FO_link[j][i].center_slice_all_dep(ka/g_ka)
+                                grad_c_k = FO_link[j][i].grad_center_slice_all_dep(ka/g_ka)/g_ka
                                 for o in range(n_obs):
                                     cons, ind = torch.max((As[j][o][i]@c_k.unsqueeze(-1)).squeeze(-1) - bs[j][o][i],-1) # shape: n_timsteps, SAFE if >=1e-6
                                     grad_cons = (As[j][o][i].gather(-2,ind.reshape(n_timesteps,1,1).repeat(1,1,dimension))@grad_c_k).squeeze(-2) # shape: n_timsteps, n_links safe if >=1e-6
@@ -109,7 +107,7 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                             cons_obs = torch.zeros(M)   
                             grad_cons_obs = torch.zeros(M,n_links)
                             # velocity min max constraints
-                            possible_max_min_q_dot = torch.vstack((ctx.qvel[i],ctx.qvel[i]+g_ka*x*T_PLAN,torch.zeros_like(ctx.qvel[i])))
+                            possible_max_min_q_dot = torch.vstack((qvel[i],qvel[i]+x*T_PLAN,torch.zeros_like(qvel[i])))
                             q_dot_max, q_dot_max_idx = possible_max_min_q_dot.max(0)
                             q_dot_min, q_dot_min_idx = possible_max_min_q_dot.min(0)
                             grad_q_max = torch.diag(T_PLAN*(q_dot_max_idx%2))
@@ -118,8 +116,8 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                             grad_cons_obs[-2*n_links:] = torch.vstack((grad_q_max,grad_q_min))
                             # velocity min max constraints 
                             for j in range(n_links):
-                                c_k = FO_link[j][i].center_slice_all_dep(ka)
-                                grad_c_k = FO_link[j][i].grad_center_slice_all_dep(ka)
+                                c_k = FO_link[j][i].center_slice_all_dep(ka/g_ka)
+                                grad_c_k = FO_link[j][i].grad_center_slice_all_dep(ka/g_ka)/g_ka
                                 for o in range(n_obs):
                                     cons, ind = torch.max((As[j][o][i]@c_k.unsqueeze(-1)).squeeze(-1) - bs[j][o][i],-1) # shape: n_timsteps, SAFE if >=1e-6
                                     grad_cons = (As[j][o][i].gather(-2,ind.reshape(n_timesteps,1,1).repeat(1,1,dimension))@grad_c_k).squeeze(-2) # shape: n_timsteps, n_links safe if >=1e-6
@@ -139,8 +137,8 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                 n = n_links,
                 m = M,
                 problem_obj=nlp_setup(),
-                lb = [-1]*n_links,
-                ub = [1]*n_links,
+                lb = [-g_ka]*n_links,
+                ub = [g_ka]*n_links,
                 cl = [1e-6]*M_obs+[-1e20]*n_links+[-torch.pi+1e-6]*n_links,
                 cu = [1e20]*M_obs+[torch.pi-1e-6]*n_links+[1e20]*n_links,
                 )
@@ -150,15 +148,12 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                 k_opt, info = NLP.solve(ka_0)
 
 
-                NLP =nlp_setup()
-                
-                import pdb;pdb.set_trace()
                 # NOTE: for training, dont care about fail-safe
                 if info['status'] == 0:
-                    ctx.lambd[i] = torch.tensor(k_opt,dtype = torch.get_default_dtype())
-                    ctx.flags[i] = 0
+                    lambd[i] = torch.tensor(k_opt,dtype = torch.get_default_dtype())/g_ka
+                    flags[i] = 0
                 else:
-                    ctx.flags[i] = 1
+                    flags[i] = 1
 
                 '''
                 # NOTE: for training, dont care about fail-safe
@@ -170,15 +165,51 @@ def gen_grad_RTS_2D_Layer(link_zonos,joint_axes,n_links,n_obs,params):
                     flags[i]=0
                 '''
             #print(f'rts pass: {flags}')
-            return ctx.lambd, FO_link, ctx.flags, info
+            return lambd, FO_link, flags
 
-        @staticmethod
-        def backward(ctx,*grad_ouput):
-            direction = grad_ouput[0]
-            rts_pass = (ctx.flags == 0).reshape(-1,1)
-            k_lim = (abs(ctx.lambd)>=1-1e-6)
-            vel_lim = (abs(ctx.qvel+ctx.lambd*g_ka*T_PLAN)>PI_vel-1e-6)
-            strongly_active = rts_pass*(k_lim+vel_lim)
-            grad_lambd = (direction*(~strongly_active)).reshape(ctx.lambd_shape)
-            return (grad_lambd,torch.zeros(ctx.obs_shape))
-    return grad_RTS_2D_Layer.apply
+        @staticmethod 
+        def backward(ctx,grad_ouput):
+            return (torch.zeros(ctx.lambd_shape),torch.zeros(ctx.obs_shape))
+
+    return RTS_star_2D_Layer.apply
+
+if __name__ == '__main__':
+    from zonopy.environments.arm_2d import Arm_2D
+    import time
+    n_links = 2
+    env = Arm_2D(n_links=n_links,n_obs=1)
+    observation = env.set_initial(qpos = torch.tensor([0.1*torch.pi,0.1*torch.pi]),qvel= torch.zeros(n_links), qgoal = torch.tensor([-0.5*torch.pi,-0.8*torch.pi]),obs_pos=[torch.tensor([-1,-0.9])])
+    
+    t_armtd = 0
+    params = {'n_joints':env.n_links, 'P':env.P0, 'R':env.R0}
+    joint_axes = [j for j in env.joint_axes]
+    RTS = gen_RTS_star_2D_Layer(env.link_zonos,joint_axes,env.n_links,env.n_obs,params)
+
+    n_steps = 30
+    for _ in range(n_steps):
+        ts = time.time()
+        observ_temp = torch.hstack([observation[key].flatten() for key in observation.keys() ])
+        #k = 2*(env.qgoal - env.qpos - env.qvel*T_PLAN)/(T_PLAN**2)
+        lam = torch.tensor([0.8,0.8])
+        lam, FO_link, flag = RTS(torch.vstack((lam,lam)),torch.vstack((observ_temp,observ_temp))) 
+        #ka, FO_link, flag = RTS(k,observ_temp)
+        print(f'action: {lam}')
+        print(f'flag: {flag}')
+            
+        t_elasped = time.time()-ts
+        print(f'Time elasped for ARMTD-2d:{t_elasped}')
+        t_armtd += t_elasped
+        #print(ka[0])
+        observation, reward, done, info = env.step(lam[0]*torch.pi/24,flag[0])
+        
+        FO_link = [fo[0] for fo in FO_link]
+        env.render(FO_link)
+        '''
+        if done:
+            import pdb;pdb.set_trace()
+            break
+        '''
+
+
+    print(f'Total time elasped for ARMTD-2d with {n_steps} steps: {t_armtd}')
+    import pdb;pdb.set_trace()
