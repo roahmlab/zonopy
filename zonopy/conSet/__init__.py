378ab5ee21349682fd8f2246a616cf9225ef807a
Yongseok Kwon <kwonys@umich.edu>
2022-06-27 14:10:05 -0400

    property id

--- zonopy/conSet/__init__.py
+++ zonopy/conSet/__init__.py
@@ -33,7 +33,7 @@ class __DefaultOptions(object):
             assert itype == torch.int or itype == torch.long or itype == torch.short, 'Default itype should be integer.'
             self.__itype = itype
         if device is not None:
-            PROPERTY_ID._to(device)
+            PROPERTY_ID.to(device)
             self.__device = device
 
 DEFAULT_OPTS = __DefaultOptions()
@@ -43,35 +43,50 @@ class __Property_ID(object):
     __dict: dict = {'None':[]}
     __properties: list = []
     __ids: torch.Tensor = torch.tensor([],dtype=torch.long)
+    __device: str = 'cpu' 
 
-    def _reset(self):
-        self.__dict = {'None':[]}
+    def _reset(self,device='cpu'):
+        self.__dict = {'None':torch.tensor([],dtype=torch.long,device=device)}
         self.__properties = []
-        self.__ids = torch.tensor([],dtype=torch.long)
+        self.__ids = torch.tensor([],dtype=torch.long,device=device)
+        self.__device = device
 
     def __getitem__(self,key): 
         assert isinstance(key,str)       
         return torch.tensor(self.__dict[key],dtype=torch.long)
+    @property 
+    def dictionary(self):
+        return self.__dict
+    @property 
+    def properties(self):
+        return self.__properties 
+    @property 
+    def ids(self):
+        return self.__ids
+    @property 
+    def device(self):
+        return self.__device    
     @property
     def offset(self):
-        return self.__ids.numel()    
+        return self.__ids.numel()
     def __repr__(self):
         return str(self.__dict)
     def __str__(self):
         return self.__repr__()
-    def _to(self,device):
+    def to(self,device):
         self.__ids = self.__ids.to(device=device)
+        for key, item in self.__dict.items():
+            self.__dict[key] = item.to(deivce=device)
 
-    def update(self,num,prop='None',device=DEFAULT_OPTS.DEVICE):
+    def update(self,num,prop='None'):
         if isinstance(prop,str):
-            new_id = torch.arange(num,dtype=torch.long) + self.offset
-            l_id = new_id.tolist()
+            new_id = torch.arange(num,dtype=torch.long,deivce=self.__device) + self.offset
             self.__ids = torch.hstack((self.__ids,new_id))
             if prop in self.__properties:
-                self.__dict[prop].extend(l_id)
+                self.__dict[prop] = torch.hstack((self.__dict[prop],new_id))
             else:
                 self.__properties.append(prop)
-                self.__dict[prop] = l_id
+                self.__dict[prop] = new_id.clone()
         '''
         elif isinstance(prop,dict):
             # prop = prop names : # gens
--- zonopy/utils/__init__.py
+++ zonopy/utils/__init__.py
@@ -2,6 +2,6 @@ from zonopy.utils.math import *
 from zonopy.utils.plot import *
 from zonopy.conSet import PROPERTY_ID
 from zonopy.utils.collision import config_safety_check, traj_safety_check, obstacle_collison_free_check
-def reset():
-    PROPERTY_ID._reset()
+def reset(deivce='cpu'):
+    PROPERTY_ID._reset(deivce)
 
