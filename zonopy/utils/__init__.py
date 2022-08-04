98082de5f6f7b77efad04bdd321aa44af21c5ec8
Yongseok Kwon <kwonys@umich.edu>
2022-08-04 17:10:08 -0400

    reset with n_ids

--- zonopy/utils/__init__.py
+++ zonopy/utils/__init__.py
@@ -2,6 +2,9 @@ from zonopy.utils.math import *
 from zonopy.utils.plot import *
 from zonopy.conSet import PROPERTY_ID
 from zonopy.utils.collision import config_safety_check, traj_safety_check, obstacle_collison_free_check
-def reset(deivce='cpu'):
+def reset(n_ids = 0, deivce='cpu'):
     PROPERTY_ID._reset(deivce)
+    if n_ids > 0:
+        PROPERTY_ID.update(n_ids)
+
 
