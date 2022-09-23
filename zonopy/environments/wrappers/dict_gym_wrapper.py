5ef9cee061b34342d0ef9967bb74ee428b6d76f3
Yongseok Kwon <kwonys@goldfinch.engin.umich.edu>
2022-09-23 14:24:10 -0400

    fix default flag for parallel_dict_gym_wrapper step

--- zonopy/environments/wrappers/dict_gym_wrapper.py
+++ zonopy/environments/wrappers/dict_gym_wrapper.py
@@ -108,7 +108,7 @@ class ParallelDictGymWrapper(DictGymWrapper):
         if len(args) > 0:
             flag = torch.tensor(args[0], dtype=int,device=self.env.device)
         else:
-            flag = None
+            flag = torch.zeros(self.num_envs,dtype=int,device=self.env.device)
         ob_dicts, rewards, dones, infos = self.env.step(torch.as_tensor(action,dtype=self.env.dtype), flag)
         for b in range(self.n_envs):
             infos[b]['action_taken'] = action[b]
